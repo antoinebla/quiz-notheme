031af39ccce08d374788958359f655a9ba9f957a
antoinebla <huhu.antoine@gmail.com>
2014-12-08 00:23:41 +0900

    2nd commit

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  classes = require('bespoke-classes'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  forms = require('bespoke-forms');
+
+// Bespoke.js
+bespoke.from('article', [
+  classes(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  hash(),
+  progress(),
+  forms()
+]);
